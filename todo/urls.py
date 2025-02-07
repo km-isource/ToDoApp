bc4de0e54220fd1c0cdb1817e465d334192e3775
km-isource <kimsykimanimburu@gmail.com>
2025-02-07 13:36:25 +0300

    Add Tasks

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('todo/addtask/', views.addTask, name='add_task' )
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Task
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    not_complete = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    complete = Task.objects.filter(is_completed=True)
+    context = {
+        'not_complete': not_complete,
+        'complete': complete,
+    }
+    return render(request, 'home.html', context)
+
+def addTask(request):
+    add_task = request.POST['task']
+    Task.objects.create(task=add_task)
+    return redirect('home')
+    
+    
