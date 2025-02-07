fde4f55bbc59235de4845ea9e332ae098961f963
km-isource <kimsykimanimburu@gmail.com>
2025-02-07 17:28:19 +0300

    CRUD

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('todo/addtask/', views.addTask, name='add_task' )
+    path('todo/add-task/', views.add_task, name='add_task' ),
+    path('todo/mark-as-done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('todo/undo-task/<int:pk>/', views.mark_as_not_done, name='mark_as_not_done'),
+    path('todo/edit-task/<int:pk>/', views.edit_task, name='edit_task'),
+    path('todo/delete-task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,19 +1,46 @@
-from django.shortcuts import redirect, render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 
 # Create your views here.
 def home(request):
+    complete = Task.objects.filter(is_completed=True).order_by('-updated_at')
     not_complete = Task.objects.filter(is_completed=False).order_by('-updated_at')
-    complete = Task.objects.filter(is_completed=True)
     context = {
-        'not_complete': not_complete,
         'complete': complete,
+        'not_complete': not_complete,
     }
     return render(request, 'home.html', context)
 
-def addTask(request):
-    add_task = request.POST['task']
+def add_task(request):
+    add_task = request.POST.get('task')
     Task.objects.create(task=add_task)
     return redirect('home')
-    
-    
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_not_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    if request.method == "POST":
+        task = get_object_or_404(Task, pk=pk)
+        new_task_value = request.POST.get("task")
+        task.task = new_task_value
+        task.save()
+        return JsonResponse({"status": "success", "task": task.task})
+    return JsonResponse({"status": "error"})
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+
